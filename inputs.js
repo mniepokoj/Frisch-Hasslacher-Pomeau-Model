3f537e1261ff777691359a79b6fb173265238fcb
mniepokoj <mniepokoj1@gmail.com>
2024-04-19 15:58:52 +0200

    update

--- inputs.js
+++ inputs.js
@@ -3,20 +3,35 @@ document.addEventListener("DOMContentLoaded", function() {
     var mode1 = document.getElementById("mode1");
     var mode2 = document.getElementById("mode2");
 
-    modeSelect.addEventListener("change", function() {
+    modeSelect.addEventListener("change", function() 
+    {
         var selectedMode = this.value;
 
-        if (selectedMode === "mode1") {
+        if (selectedMode === "mode1") 
+        {
             mode1.style.display = "block";
             mode2.style.display = "none";
-        } else {
+
+            var inputs = document.querySelectorAll('.customInput');
+
+            number = 0;
+            for(var i = 0; i < 32; i++)
+            {
+                number += inputs[i].value * Math.pow(2, i);
+            }
+
+            document.getElementById("automatonNumber").value = number;
+        } 
+        else 
+        {
             mode1.style.display = "none";
             mode2.style.display = "block";
-            generateInputs();
+            inputVal = document.getElementById("automatonNumber").value;
+            generateInputs(inputVal);
         }    
     });
 
-    function generateInputs() 
+    function generateInputs(value) 
     {
         inputVal = document.getElementById("automatonNumber").value;
 
@@ -27,7 +42,8 @@ document.addEventListener("DOMContentLoaded", function() {
             var row = document.createElement("div");
             row.classList.add("inputRow");
 
-            for (var j = 0; j < 8; j++) {
+            for (var j = 0; j < 8; j++) 
+            {
                 var inputValue = ((i * 8) + j).toString(2).padStart(5, '0');
 
                 var inputDescription = document.createElement("span");
@@ -39,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
                 var input = document.createElement("input");
                 input.type = "number";
-                input.value = 0;
+                input.value = (value >> i*8+j) & 1;
                 input.min = 0;
                 input.max = 1;
                 input.pattern = "[0-1]";
