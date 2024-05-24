062fb88d9597b251ed6225fcc42aca8d6adf626b
mniepokoj1 <mniepokoj1@gmail.com>
2024-05-24 15:29:11 +0200

    add logic board

--- animation.js
+++ animation.js
@@ -8,42 +8,117 @@ canvas.height = window.innerHeight;
 var width = canvas.width;
 var height = canvas.height;
 
-var boardSize = 20;
+var boardSize = 50;
 var cellSize = new Point(width / (boardSize * 2), height / (boardSize * 2));
+var particleMargin = 100;
+var particleDensityFactor = 0.2;
 
-var hexBoard = [];
+var particleElements = [];
+var wallElements = [];
 var orientation = layout_flat;
 var layout = new Layout(orientation, cellSize, new Point(width / 2, height / 2));
 
-function initBoard() {    
-    let margin = 50;
-    let leftBoundary = margin;
-    let rightBoundary = width - margin;
-    let topBoundary = margin;
-    let bottomBoundary = height - margin;
 
-    for (let r = -boardSize; r <= boardSize; r++) 
+    class Element
     {
-        for (let q = -boardSize; q <= boardSize; q++) {
-            let s = -r - q;
+        static s_empty = "empty";
+        static s_particle = "particle";
+        static s_wall = "wall";
+        static s_id = 0;
 
-            let hex = new Hex(q, r, s);
-            let point = hex_to_pixel(layout, hex);
+        type = "particle";
 
-            if (point.x >= leftBoundary && point.x <= rightBoundary && point.y >= topBoundary && point.y <= bottomBoundary) {
-                hexBoard.push(hex);
+        coord = Hex(0, 0, 0);
+        speed = Hex(0, 0, 0);
+        id = 0;
+
+        constructor(type, coord, speed) 
+        {
+            this.type = type;
+            this.coord = coord;
+            this.speed = speed;
+            id = s_id;
+            s_id += 1;
+        }
+    }
+
+    function initElements()
+    {
+        let leftBoundary = particleMargin;
+        let rightBoundary = width - particleMargin;
+        let topBoundary = particleMargin;
+        let bottomBoundary = height - particleMargin;
+
+        for (let r = -boardSize; r <= boardSize; r++) 
+        {
+            for (let q = -boardSize; q <= boardSize; q++) {
+                let s = -r - q;
+
+                let hex = new Hex(q, r, s);
+                let point = hex_to_pixel(layout, hex);
+
+                if (point.x >= leftBoundary && point.x <= rightBoundary && point.y >= topBoundary && point.y <= bottomBoundary) 
+                {
+                    let random = Math.random();
+                    let type = Element.s_empty;
+                    if(random < particleDensityFactor)
+                    {
+                        type = Element.s_particle;
+                    }
+                    let speed = Hex(0 ,0, 0);
+                    particleElements.push(new Element(type, hex, speed));
+                }
+                else
+                {
+                    let type = Element.s_wall;
+                    let speed = Hex(0 ,0, 0);
+                    particleElements.push(new Element(type, hex, speed));
+                }
             }
         }
     }
-}
+
+    function initBoard() 
+    {    
+        initElements();
+    }
+
+    function getColor(element)
+    {
+        let color = 'rgba(128, 128, 28, 255)';
+        switch(element.type)
+        {
+            case Element.s_empty:
+                color = 'rgba(0, 0, 0, 0)';
+                break;
+            case Element.s_particle:
+                color = 'rgba(255, 0, 0, 255)';
+                break;
+            case Element.s_wall:
+                color = 'rgba(0, 0, 0, 255)';
+                break;   
+        }
+        return color;
+    }
+
+    function drawElement(element)
+    {
+        let point = hex_to_pixel(layout, element.coord);
+        ctx.fillStyle = getColor(element);
+        let pointSize = 10;
+        ctx.fillRect(point.x - pointSize / 2, point.y - pointSize / 2, pointSize, pointSize);
+    }
 
     function drawChessboard() 
     {
-        for (const hexElement of hexBoard) {
-            let point = hex_to_pixel(layout, hexElement);
-            ctx.fillStyle = 'rgba(128, 128, 28, 255)';
-            let pointSize = 20;
-            ctx.fillRect(point.x - pointSize / 2, point.y - pointSize / 2, pointSize, pointSize);
+        for (const element of particleElements) 
+        {
+            drawElement(element);
+        }
+
+        for (const element of wallElements) 
+        {
+            drawElement(element);
         }
     }
 
@@ -51,6 +126,19 @@ function initBoard() {
     {
         drawChessboard();
     }
+
+
+
+    function animate(elemets)
+    {
+        if(elemets.length == 2)
+        {
+            elem1 = elemets[0];
+            elem2 = elemets[1];
+            if(elemets[])
+        }
+    }
+
 
     function animateChessboard() 
     {
